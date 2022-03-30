ad26ed1d77cf5ef9af94fb190c8f23d0c151a830
hlukhau <dzmitry.hlukhau@softclub.by>
2022-03-30 15:23:12 +0300

    two clients

--- client2.py
+++ client2.py
@@ -5,7 +5,7 @@ import numpy as np
 
 context = zmq.Context()
 footage_socket = context.socket(zmq.SUB)
-footage_socket.bind('tcp://*:5555')
+footage_socket.bind('tcp://*:5556')
 footage_socket.setsockopt_string(zmq.SUBSCRIBE, np.compat.unicode(''))
 
 while True:
--- server.py
+++ server.py
@@ -7,16 +7,24 @@ context = zmq.Context()
 footage_socket = context.socket(zmq.PUB)
 footage_socket.connect('tcp://localhost:5555')
 
+footage_socket2 = context.socket(zmq.PUB)
+footage_socket2.connect('tcp://localhost:5556')
+
 start_time = time.time()
 camera = cv2.VideoCapture('3.mp4')  # init the camera
+w = int(camera.get(3) / 2)
+h = int(camera.get(4) / 2)
+
+print(w, h)
 
 while True:
     try:
         grabbed, frame = camera.read()  # grab the current frame
-        frame = cv2.resize(frame, (640, 480))  # resize the frame
+        frame = cv2.resize(frame, (w, h))  # resize the frame
         encoded, buffer = cv2.imencode('.jpg', frame)
         jpg_as_text = base64.b64encode(buffer)
         footage_socket.send(jpg_as_text)
+        footage_socket2.send(jpg_as_text)
 
         elapsed = (time.time() - start_time) * 1000  # msec
         play_time = int(camera.get(cv2.CAP_PROP_POS_MSEC))

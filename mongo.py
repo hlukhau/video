7d7682387c300cdd037b9ee3b6886c7b771f6784
hlukhau <dzmitry.hlukhau@softclub.by>
2022-04-05 11:25:08 +0300

    mongo all command

--- mongo.py
+++ mongo.py
@@ -2,27 +2,39 @@ from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017/afm')
 db = client["afm"]
+db.projects.drop()
+
 serverStatusResult = db.command("serverStatus")
 
 print(serverStatusResult)
-
-db = client.business
 
 project01 = {
 "_id" : "1",
 "name" : "Project 1",
 "author" : "Hlukhau Dzmitry",
-"category" : "Advertising"
+"category" : "Advertising",
+"sum" : 101
 }
 
 project02 = {
 "_id" : "2",
 "name" : "Project 2",
 "author" : "Hlukhau Dzmitry",
-"category" : "Advertising"
+"category" : "Advertising",
+"sum" : 214
+}
+
+project03 = {
+"_id" : "3",
+"name" : "Project 3",
+"author" : "Hlukhau Dzmitry",
+"category" : "Book",
+"sum" : 143
 }
 
 db.projects.insert_one(project01)
+db.projects.insert_one(project02)
+db.projects.insert_one(project03)
 
 myquery = { "_id": "1" }
 newvalues = { "$set": { "name": "Project 01" } }
@@ -47,5 +59,13 @@ mydoc = db.projects.find().sort("name", 1)
 for x in mydoc:
   print(x)
 
+
+pipeline = [
+    {"$group": {"_id": "$category", "sum": {"$sum": "$sum"}}},
+]
+
+print(list(db.projects.aggregate(pipeline)))
+
 myquery = { "author": "Hlukhau Dzmitry" }
 db.projects.delete_one(myquery)
+

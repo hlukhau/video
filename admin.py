3840540eef28fce8387372a38ebce4f6dce44a87
hlukhau <dzmitry.hlukhau@softclub.by>
2022-04-11 16:55:29 +0300

    contents buttons prepared

--- admin.py
+++ admin.py
@@ -40,11 +40,9 @@ def content():
         scene = '/static/projects/demo/scene/scene.jpg'
         print(scene)
 
-        if (os.path.exists('static/projects/demo/video/small.jpg')):
-            video = '/static/projects/demo/video/small.jpg'
-            print(video)
-    else:
-        print('no scene')
+    if (os.path.exists('static/projects/demo/video/small.jpg')):
+        video = '/static/projects/demo/video/small.jpg'
+        print(video)
 
     print("Session project: " + session.get('project'))
 
@@ -60,15 +58,41 @@ def hello():
 
 @app.route('/upload')
 def upload_html():
-    return render_template('upload.html')
+    path = request.args.get('path')
+    return render_template('upload.html', path=path)
 
 @app.route('/send', methods=['GET', 'POST'])
 def send_file():
+    path = request.args.get('path')
+    print("send: " + path)
+
     if request.method == 'POST':
         f = request.files['file']
-        f.save('files/' + f.filename)
-        return "file uploaded successfully"
+        print("path: " + path + f.filename)
 
+        if path.find("scene") != -1:
+            print("save")
+            f.save("static/projects/demo/scene/scene.jpg")
+
+        if path.find("video") != -1:
+            f.save("static/projects/demo/video/video.mp4")
+            sub = cv2.VideoCapture('files/' + f.filename)
+            sub.set(cv2.CAP_PROP_POS_MSEC, 2000)  # just cue to 20 sec. position
+            success, image = sub.read()
+
+            if success:
+                print('success')
+                h, w, _ = image.shape
+                width = int(w)
+                height = int(h)
+                height = int(128 * height / width)
+                width = 128
+                print(w, h)
+                print(width, height)
+                resized = cv2.resize(image, (width, height))
+                cv2.imwrite("static/projects/demo/video/small.jpg", resized)  # save frame as JPEG file
+
+        return redirect('/')
 
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
@@ -90,7 +114,28 @@ def upload_file():
             resized = cv2.resize(image, (width, height))
             cv2.imwrite('static/' + f.filename + ".jpg", image)  # save frame as JPEG file
             cv2.imwrite('static/' + f.filename + "s.jpg", resized)  # save frame as JPEG file
-        return "file uploaded successfully<br /><img src='" + 'static/' + f.filename + "s.jpg" + "'><br><img src='" + 'static/' + f.filename + ".jpg" + "'>"
+        return redirect('/')
+
+
+@app.route('/drag-drop')
+def drop_down_upload():
+    path = request.args.get('path')
+    message = request.args.get('message')
+    return render_template('drag-drop.html', path=path, message=message)
+
+
+@app.route('/remove')
+def remove():
+    path = request.args.get('path')
+
+    if path.find("scene") != -1:
+        os.remove("static/projects/demo/scene/scene.jpg")
+
+    if path.find("video") != -1:
+        os.remove("static/projects/demo/video/small.jpg")
+        os.remove("static/projects/demo/video/video.mp4")
+
+    return redirect('/')
 
 
 @app.route('/exit')
@@ -98,7 +143,10 @@ def exit():
     session.pop('project', None)
     return redirect('/')
 
+
 if __name__ == '__main__':
     app.config['UPLOAD_FOLDER'] = 'files'
     app.run(host='0.0.0.0', port=5555)
+
+
 

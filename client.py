37af38b7ffd12fe847bc63ceb33ba02ce1e93b16
hlukhau <dzmitry.hlukhau@softclub.by>
2022-05-26 17:01:05 +0300

    universal client

--- client.py
+++ client.py
@@ -5,56 +5,70 @@ import numpy as np
 import sys
 import logging
 
-print(sys.argv[1])
-
-logging.basicConfig(level=logging.ERROR)
-
-
-first = 1
-waitImage = cv2.imread('static/wait.webp')
-
-port = sys.argv[1]
-name_stream = "Stream:" + port
-name_waiting = "Listening:" + port
-
-context = zmq.Context()
-receiver = context.socket(zmq.SUB)
-receiver.bind('tcp://*:' + port)
-receiver.setsockopt_string(zmq.SUBSCRIBE, np.compat.unicode(''))
-receiver.setsockopt(zmq.CONFLATE, 1)  # last msg only.
-receiver.setsockopt(zmq.RCVTIMEO, 1000)
-
-cv2.imshow(name_waiting, waitImage)
-cv2.waitKey(100)
+port = ''
+fullscreen = 'no'
+offset = 0
 
 try:
-    offset = int(sys.argv[2])
+    port = sys.argv[1]
+    fullscreen = int(sys.argv[2])
+    offset = int(sys.argv[3])
 except:
     offset = 0
 
-while True:
-    try:
-        frame = receiver.recv_string()
 
-        if first == 1:
-            position = cv2.getWindowImageRect(name_waiting)
-            cv2.destroyWindow(name_waiting)
+def listen(port, fullscreen, offset):
+    logging.basicConfig(level=logging.ERROR)
 
-        first = 0
-        img = base64.b64decode(frame)
-        npimg = np.frombuffer(img, dtype=np.uint8)
-        source = cv2.imdecode(npimg, 1)
-        cv2.namedWindow(name_stream, cv2.WND_PROP_FULLSCREEN)
-        cv2.setWindowProperty(name_stream, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_AUTOSIZE)
-        cv2.imshow(name_stream, source)
-        cv2.moveWindow(name_stream, offset, 0)
-    except:
-        if first == 0:
-            cv2.destroyWindow(name_stream)
-            cv2.imshow(name_waiting, waitImage)
-            cv2.moveWindow(name_waiting, position[0], position[1])
-            first = 1
+    first = 1
+    waitImage = cv2.imread('static/wait.webp')
 
-    if cv2.waitKey(1) & 0xff == 27:
-        cv2.destroyAllWindows()
-        break
+
+    print('Starting client proceess at ' + str(port) + ' port')
+
+    name_stream = "Stream:" + port
+    name_waiting = "Listening:" + port
+
+    context = zmq.Context()
+    receiver = context.socket(zmq.SUB)
+    receiver.setsockopt_string(zmq.SUBSCRIBE, np.compat.unicode(''))
+    receiver.setsockopt(zmq.CONFLATE, 1)  # last msg only.
+    receiver.setsockopt(zmq.RCVTIMEO, 1000)
+    receiver.bind('tcp://*:' + port)
+
+    cv2.imshow(name_waiting, waitImage)
+
+    while True:
+        try:
+            frame = receiver.recv_string()
+
+            if first == 1:
+                position = cv2.getWindowImageRect(name_waiting)
+                cv2.destroyWindow(name_waiting)
+
+            first = 0
+            img = base64.b64decode(frame)
+            npimg = np.frombuffer(img, dtype=np.uint8)
+            source = cv2.imdecode(npimg, 1)
+
+            if fullscreen == 'fullscreen':
+                cv2.namedWindow(name_stream, cv2.WND_PROP_FULLSCREEN)
+                cv2.setWindowProperty(name_stream, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_AUTOSIZE)
+
+            cv2.imshow(name_stream, source)
+
+            if fullscreen == 'fullscreen':
+                cv2.moveWindow(name_stream, offset, 0)
+        except:
+            if first == 0:
+                cv2.destroyWindow(name_stream)
+                cv2.imshow(name_waiting, waitImage)
+                cv2.moveWindow(name_waiting, position[0], position[1])
+                first = 1
+
+        if cv2.waitKey(1) & 0xff == 27:
+            cv2.destroyAllWindows()
+            break
+
+
+listen(port, fullscreen, offset)
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-import subprocess
+from multiprocessing import Process
+from client import listen
 
-subprocess.Popen(["python", "/home/hlukhau/PycharmProjects/video/client.py", "5556"])
-subprocess.Popen(["python", "/home/hlukhau/PycharmProjects/video/client.py", "5557"])
+# import subprocess
+#
+# subprocess.Popen(["python", "/home/hlukhau/PycharmProjects/video/client.py", "5556"])
+# subprocess.Popen(["python", "/home/hlukhau/PycharmProjects/video/client.py", "5557"])
+# subprocess.Popen(["python", "/home/hlukhau/PycharmProjects/video/client.py", "5558"])
+
+proc1 = Process(target=listen, args=('5556', '', ''))
+proc1.start()
+
+proc2 = Process(target=listen, args=('5557', '', ''))
+proc2.start()
+
+proc3 = Process(target=listen, args=('5558', '', ''))
+proc3.start()
+
 
 
